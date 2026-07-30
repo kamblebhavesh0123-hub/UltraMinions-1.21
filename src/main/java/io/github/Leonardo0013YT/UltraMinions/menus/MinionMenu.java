bc6a0859dab5a9f9c11ff9712c92a8b27db1a351
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-30 23:21:57 +0530

    Add files via upload

--- src/main/java/io/github/Leonardo0013YT/UltraMinions/menus/MinionMenu.java
+++ src/main/java/io/github/Leonardo0013YT/UltraMinions/menus/MinionMenu.java
@@ -315,4 +315,222 @@ public class MinionMenu {
                }
             }
 
-            ItemStack quick = ItemBuilder.item(Material.DIAMOND, 1, (short)0, this.plugin.getLang().get("menus.minion.quick.nameItem"), this.plugin.getLang().get("menus.minion.quick.loreItem").replaceAll("<status>", has ? this.plugin.getLang().get("menus.minion.quick.has") : this.plug
+            ItemStack quick = ItemBuilder.item(Material.DIAMOND, 1, (short)0, this.plugin.getLang().get("menus.minion.quick.nameItem"), this.plugin.getLang().get("menus.minion.quick.loreItem").replaceAll("<status>", has ? this.plugin.getLang().get("menus.minion.quick.has") : this.plugin.getLang().get("menus.minion.quick.noHas")).replaceAll("<slotsNow>", String.valueOf(mn.getMax())).replaceAll("<timeNow>", String.valueOf(mn.getDelay())).replaceAll("<slotsNext>", mn.equals(ml) ? "§cMax" : String.valueOf(ml.getMax())).replaceAll("<timeNext>", mn.equals(ml) ? "§cMax" : String.valueOf(ml.getDelay())));
+            inv.setItem(50, quick);
+         }
+      }
+
+      boolean special = pm.getType().equals(MinionType.COLLECTOR) || pm.getType().equals(MinionType.SELLER);
+      if (!special) {
+         for(int i = 0; i < this.inv.size(); ++i) {
+            if (i >= slot) {
+               inv.setItem((Integer)this.inv.get(i), white);
+            }
+         }
+      } else {
+         for(int integer : this.inv) {
+            inv.setItem(integer, chest);
+         }
+      }
+
+      if (!pm.getSkin().equals("none") && this.plugin.getSkm().getSkins().get(pm.getSkin()) != null) {
+         inv.setItem(10, ((MinionSkin)this.plugin.getSkm().getSkins().get(pm.getSkin())).getHead());
+      } else {
+         inv.setItem(10, skin);
+      }
+
+      if (!m.isFuelEnabled()) {
+         inv.setItem(19, black);
+      } else if (pm.getUpgrade().getFuel() != null) {
+         boolean ended = this.ended(pm.getStat(), pm.getUpgrade().getFuel());
+         if (ended) {
+            inv.setItem(19, fuel);
+         } else {
+            ItemStack i = (ItemStack)NBTEditor.set(pm.getUpgrade().getFuel().getResult(true, this.getTime(pm.getStat(), pm.getUpgrade().getFuel())), pm.getUpgrade().getFuel().getName(), "MINION", "FUEL", "TIME");
+            i.setAmount(pm.getStat().getAmountFuel(pm.getUpgrade().getFuel()));
+            inv.setItem(19, i);
+         }
+      } else {
+         inv.setItem(19, fuel);
+      }
+
+      if (!m.isAutoSellEnabled()) {
+         inv.setItem(28, black);
+      } else if (pm.getUpgrade().getAutoSell() != null) {
+         inv.setItem(28, pm.getUpgrade().getAutoSell().getResult());
+      } else {
+         inv.setItem(28, shipping);
+      }
+
+      if (!m.isCompressorEnabled()) {
+         inv.setItem(37, black);
+      } else if (pm.getUpgrade().getCompressor() != null) {
+         inv.setItem(37, pm.getUpgrade().getCompressor().getResult());
+      } else {
+         inv.setItem(37, update);
+      }
+
+      if (!m.isSmeltEnabled()) {
+         inv.setItem(46, black);
+      } else if (pm.getUpgrade().getAutoSmelt() != null) {
+         inv.setItem(46, pm.getUpgrade().getAutoSmelt().getResult());
+      } else {
+         inv.setItem(46, update);
+      }
+
+      inv.setItem(3, layout);
+      inv.setItem(4, info);
+      inv.setItem(5, next);
+      inv.setItem(48, collect);
+      inv.setItem(53, pickup);
+      if (!special) {
+         for(Map.Entry<ItemStack, Integer> entry : pm.getItems().entrySet()) {
+            if (entry.getKey() != null && !((ItemStack)entry.getKey()).getType().equals(Material.AIR)) {
+               int stack = (Integer)entry.getValue() / 64;
+               int rest = (Integer)entry.getValue() % 64;
+
+               for(int i = 0; i < stack; ++i) {
+                  ItemStack item = ((ItemStack)entry.getKey()).clone();
+                  item.setAmount(64);
+                  inv.addItem(new ItemStack[]{item});
+               }
+
+               if (rest > 0) {
+                  ItemStack add = ((ItemStack)entry.getKey()).clone();
+                  add.setAmount(rest);
+                  inv.addItem(new ItemStack[]{add});
+               }
+            }
+         }
+      }
+
+      p.openInventory(inv);
+   }
+
+   public void createMinionLevelMenu(Player p, PlayerMinion pm, MinionLevel next) {
+      Inventory inv = Bukkit.createInventory((InventoryHolder)null, 27, this.plugin.getLang().get("menus.upgrade.title").replaceAll("<title>", pm.getMinion().getTitle()));
+      ItemStack level = ItemBuilder.item(Material.EXPERIENCE_BOTTLE, this.plugin.getLang().get("menus.upgrade.level.nameItem"), this.plugin.getLang().get("menus.upgrade.level.loreItem").replaceAll("<amount>", String.valueOf(next.getUpgradeLevels())).replaceAll("<level>", Utils.IntegerToRomanNumeral(next.getLevel())));
+      ItemStack coins = ItemBuilder.item(Material.SUNFLOWER, this.plugin.getLang().get("menus.upgrade.coins.nameItem"), this.plugin.getLang().get("menus.upgrade.coins.loreItem").replaceAll("<amount>", String.valueOf(next.getUpgradeCoins())).replaceAll("<level>", Utils.IntegerToRomanNumeral(next.getLevel())));
+      ItemStack craft = ItemBuilder.item(Material.CRAFTING_TABLE, this.plugin.getLang().get("menus.upgrade.craft.nameItem"), this.plugin.getLang().get("menus.upgrade.craft.loreItem"));
+      int u = next.getUpgrades();
+      if (u == 3) {
+         inv.setItem(this.m1m1m1[0], level);
+         inv.setItem(this.m1m1m1[1], coins);
+         inv.setItem(this.m1m1m1[2], craft);
+      } else if (u == 2) {
+         if (next.isCoins() && next.isLevel()) {
+            inv.setItem(this.m1m1[0], coins);
+            inv.setItem(this.m1m1[1], level);
+         } else if (next.isCoins() && next.isCraft()) {
+            inv.setItem(this.m1m1[0], coins);
+            inv.setItem(this.m1m1[1], craft);
+         } else {
+            inv.setItem(this.m1m1[0], level);
+            inv.setItem(this.m1m1[1], craft);
+         }
+      } else if (next.isCoins()) {
+         inv.setItem(this.m1[0], coins);
+      } else if (next.isLevel()) {
+         inv.setItem(this.m1[0], level);
+      } else {
+         inv.setItem(this.m1[0], craft);
+      }
+
+      p.openInventory(inv);
+   }
+
+   public void createPreviewCraftMenu(Player p, PlayerMinion pm, MinionLevel l) {
+      Inventory inv = Bukkit.createInventory((InventoryHolder)null, 54, this.plugin.getLang().get("menus.craft.title").replaceAll("<title>", pm.getMinion().getTitle()));
+      ItemStack orange = ItemBuilder.item(Material.ORANGE_STAINED_GLASS_PANE, 1, (short)15, "§7", "§7");
+      ItemStack upgrade = ItemBuilder.item(Material.SUNFLOWER, 1, (short)0, this.plugin.getLang().get("menus.craft.upgrade.nameItem"), this.plugin.getLang().get("menus.craft.upgrade.loreItem"));
+      Craft craft = l.getCraft();
+
+      for(int i = 0; i < 54; ++i) {
+         if (!this.workbench.contains(i)) {
+            inv.setItem(i, orange);
+         }
+      }
+
+      ItemStack[] m = craft.getMatrix();
+      inv.setItem(12, m[0]);
+      inv.setItem(13, m[1]);
+      inv.setItem(14, m[2]);
+      inv.setItem(21, m[3]);
+      inv.setItem(22, m[4]);
+      inv.setItem(23, m[5]);
+      inv.setItem(30, m[6]);
+      inv.setItem(31, m[7]);
+      inv.setItem(32, m[8]);
+      inv.setItem(49, upgrade);
+      p.openInventory(inv);
+   }
+
+   public void createMinionStat(Player p, PlayerMinion pm) {
+      Inventory inv = Bukkit.createInventory((InventoryHolder)null, 27, this.plugin.getLang().get("menus.stat.title"));
+      ItemStack chest;
+      if (pm.getChest() != null) {
+         chest = ItemBuilder.item(Material.CHEST, this.plugin.getLang().get("menus.stat.chest.nameItem"), this.plugin.getLang().get("menus.stat.chest.loreItem").replaceAll("<chest>", Utils.getFormatedLocation(pm.getChest().getLoc())));
+      } else {
+         chest = ItemBuilder.item(Material.CHEST, this.plugin.getLang().get("menus.stat.chest.nameItem"), this.plugin.getLang().get("menus.stat.chest.loreItem").replaceAll("<chest>", "§cNot set!"));
+      }
+
+      if (this.plugin.getCfm().isFood()) {
+         inv.setItem(11, ItemBuilder.item(Material.BREAD, this.plugin.getLang().get("menus.stat.food.nameItem"), this.plugin.getLang().get("menus.stat.food.loreItem").replaceAll("<max>", String.valueOf(pm.getMinionLevel().getFood())).replaceAll("<food>", String.valueOf(pm.getStat().getFood()))));
+      }
+
+      if (this.plugin.getCfm().isHealth()) {
+         inv.setItem(15, ItemBuilder.item(Material.APPLE, this.plugin.getLang().get("menus.stat.health.nameItem"), this.plugin.getLang().get("menus.stat.health.loreItem").replaceAll("<max>", String.valueOf(pm.getMinionLevel().getHealth())).replaceAll("<health>", String.valueOf(pm.getStat().getHealth()))));
+      }
+
+      if (this.plugin.getCfm().isChestLink()) {
+         inv.setItem(13, chest);
+      }
+
+      p.openInventory(inv);
+   }
+
+   public boolean isInv(int slot) {
+      return this.inv.contains(slot);
+   }
+
+   public String getTime(PlayerMinionStat stat, UpgradeFuel uf) {
+      long passed = System.currentTimeMillis() - stat.getFuel();
+      long restant = uf.getDuration() * (long)stat.getAmountFuel(uf) - passed;
+      int seconds = (int)restant / 1000;
+      return Utils.convertTime(seconds);
+   }
+
+   public boolean ended(PlayerMinionStat stat, UpgradeFuel uf) {
+      long passed = System.currentTimeMillis() - stat.getFuel();
+      long restant = uf.getDuration() * (long)stat.getAmountFuel(uf) - passed;
+      return restant < 0L;
+   }
+
+   private List<String> getInfoLore(PlayerMinion pm) {
+      Minion m = pm.getMinion();
+      MinionLevel ml = pm.getMinionLevel();
+      List<String> lore = new ArrayList();
+
+      for(String s : this.plugin.getLang().get("menus.minion.info.loreItem").split("\\n")) {
+         if (s.equalsIgnoreCase("<lore>")) {
+            m.getLore().forEach((l) -> lore.add(l.replaceAll("<type>", ml.getLevelTitle()).replaceAll("<generated>", String.valueOf(pm.getStat().getGenerated())).replaceAll("<max>", String.valueOf(ml.getMax())).replaceAll("<time>", String.valueOf(pm.getDelay()))));
+         } else {
+            lore.add(s.replaceAll("<type>", ml.getLevelTitle()).replaceAll("<generated>", String.valueOf(pm.getStat().getGenerated())).replaceAll("<max>", String.valueOf(ml.getMax())).replaceAll("<time>", String.valueOf(pm.getDelay())));
+         }
+      }
+
+      return lore;
+   }
+
+   public HashMap<Player, Integer> getPages() {
+      return this.pages;
+   }
+
+   public void addPage(Player p) {
+      this.pages.put(p, (Integer)this.pages.get(p) + 1);
+   }
+
+   public void removePage(Player p) {
+      this.pages.put(p, (Integer)this.pages.get(p) - 1);
+   }
+}
